b7b0ab0844fe487437c736564178c20a951040f3
mimarque <6577899+mimarque@users.noreply.github.com>
2024-08-20 03:32:01 +0100

    save4

---
+++ iscntrl.c
@@ -0,0 +1,8 @@
+#include <mylibc.h>
+
+/// @brief      Check if the character is a control character
+/// @param c    The character to check
+/// @return     Non-zero if the character is a control character, 0 otherwise
+int iscntrl(int c){
+    return (c >= 0 && c <= 31) || c == 127;
+}
---
+++ isgraph.c
@@ -0,0 +1,8 @@
+#include <mylibc.h>
+
+/// @brief      Check if the character is a printable character
+/// @param c    The character to check
+/// @return     Non-zero if the character is a printable character, 0 otherwise
+int isgraph(int c){
+    return c > ' ' && c < 127;
+}

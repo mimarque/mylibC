2b615b9bb187bd6beab3d86cb52ad3623eb786a8
mimarque <mimarque@example.com>
2024-08-20 02:48:34 +0100

    initial submit

---
+++ isalnum.c
@@ -0,0 +1,8 @@
+#include <mylib.h>
+
+/// @brief  Check if the character is an alphanumeric character
+/// @param c    The character to check
+/// @return     Non-zero if the character is an alphanumeric character, 0 otherwise
+int isalnum(int c){
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+}
---
+++ isalpha.c
@@ -0,0 +1,8 @@
+#include <mylib.h>
+
+/// @brief Check if the character is an alphabetic character
+/// @param c    The character to check
+/// @return     Non-zero if the character is an alphabetic character, 0 otherwise
+int isalpha(int c){
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
